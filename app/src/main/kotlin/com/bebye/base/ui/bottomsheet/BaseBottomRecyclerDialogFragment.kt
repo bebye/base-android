56eb8522fff06da36c1cf6bbaa7a0ec7756cf5bd
mkwon <developer.mkwon@gmail.com>
2020-06-27 20:26:41 +0900

    rename

--- app/src/main/kotlin/com/bebye/base/ui/bottomsheet/BaseBottomRecyclerDialogFragment.kt
+++ app/src/main/kotlin/com/bebye/base/ui/bottomsheet/BaseBottomRecyclerDialogFragment.kt
@@ -6,14 +6,14 @@ import androidx.annotation.CallSuper
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.RecyclerView
 import com.bebye.base.R
-import com.bebye.base.databinding.FragmentBaseBottomRecyclerDialogBinding
+import com.bebye.base.databinding.FragmentBottomRecyclerDialogBinding
 
 /**
  * Created by mkwon on 27/06/2020.
  */
-abstract class BaseBottomRecyclerDialogFragment : BaseBottomDialogFragment<FragmentBaseBottomRecyclerDialogBinding>() {
+abstract class BaseBottomRecyclerDialogFragment : BaseBottomDialogFragment<FragmentBottomRecyclerDialogBinding>() {
 
-    override var layoutResourceId = R.layout.fragment_base_bottom_recycler_dialog
+    override var layoutResourceId = R.layout.fragment_bottom_recycler_dialog
 
     protected abstract fun createLayoutManager(): RecyclerView.LayoutManager
 
