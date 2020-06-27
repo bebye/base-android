79b71f9b02a79e2c3fcdcf311dd802f038bbc2d7
mkwon <developer.mkwon@gmail.com>
2020-06-27 19:07:25 +0900

    add scale util

---
+++ app/src/main/kotlin/com/bebye/base/utils/ScaleUtil.kt
@@ -0,0 +1,21 @@
+package com.bebye.base.utils
+
+import android.util.TypedValue
+import androidx.annotation.DimenRes
+import com.bebye.base.appContext
+
+/**
+ * Created by mkwon on 27/06/2020.
+ */
+object ScaleUtils {
+
+    fun dp2px(dp: Float): Int {
+        val displayMetrics = appContext.resources.displayMetrics
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics).toInt()
+    }
+
+    fun dimen2px(@DimenRes dimension: Int): Int {
+        return appContext.resources.getDimensionPixelSize(dimension)
+    }
+
+}
