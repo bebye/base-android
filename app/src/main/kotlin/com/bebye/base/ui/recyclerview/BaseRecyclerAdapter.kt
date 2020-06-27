ce439ff85f1b514e19c2d1010df1c82158031589
mkwon <developer.mkwon@gmail.com>
2020-06-27 19:48:13 +0900

    add base recycler adapter and viewholder

---
+++ app/src/main/kotlin/com/bebye/base/ui/recyclerview/BaseRecyclerAdapter.kt
@@ -0,0 +1,18 @@
+package com.bebye.base.ui.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by mkwon on 27/06/2020.
+ */
+abstract class BaseRecyclerAdapter<T> : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var items = mutableListOf<T>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun getItemCount() = items.size
+
+}
---
+++ app/src/main/kotlin/com/bebye/base/ui/recyclerview/BaseRecyclerViewHolder.kt
@@ -0,0 +1,18 @@
+package com.bebye.base.ui.recyclerview
+
+import androidx.annotation.CallSuper
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by mkwon on 27/06/2020.
+ */
+class BaseRecyclerViewHolder<VB : ViewDataBinding, ITEM>(private val dataBinding: VB) : RecyclerView.ViewHolder(dataBinding.root) {
+
+    @CallSuper
+    fun bind(itemBindingVariableId: Int, item: ITEM) {
+        dataBinding.setVariable(itemBindingVariableId, item)
+        dataBinding.executePendingBindings()
+    }
+
+}
