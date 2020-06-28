0508e609a009fcf0834e27fedb41fd317811c629
mkwon <developer.mkwon@gmail.com>
2020-06-28 14:35:50 +0900

    update scaleutil

--- base/src/main/kotlin/com/bebye/base/utils/ScaleUtil.kt
+++ base/src/main/kotlin/com/bebye/base/utils/ScaleUtil.kt
@@ -9,6 +9,11 @@ import com.bebye.base.appContext
  */
 object ScaleUtils {
 
+    fun dp2px(dp: Int): Int {
+        val displayMetrics = appContext.resources.displayMetrics
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), displayMetrics).toInt()
+    }
+
     fun dp2px(dp: Float): Int {
         val displayMetrics = appContext.resources.displayMetrics
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics).toInt()
