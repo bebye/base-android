52ed8ccde01dc199cc95a19bc8c45accf922f1b8
mkwon <developer.mkwon@gmail.com>
2020-09-15 15:39:38 +0900

    update viewmodel extension

--- base/src/main/kotlin/com/bebye/base/extension/ViewModelExtension.kt
+++ base/src/main/kotlin/com/bebye/base/extension/ViewModelExtension.kt
@@ -6,33 +6,19 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStoreOwner
 import com.bebye.base.viewmodel.BaseViewModel
 
 /**
  * Created by mkwon on 27/06/2020.
  */
-inline fun <reified VM : BaseViewModel> ViewModelStoreOwner.createViewModel(viewModelFactory: ViewModelProvider.Factory? = null): VM {
-    return viewModelFactory?.let { ViewModelProvider(this, it).get(VM::class.java) }
-        ?: ViewModelProvider(this).get(VM::class.java)
+inline fun <reified VM : BaseViewModel> ComponentActivity.getViewModels(viewModelFactory: ViewModelProvider.Factory? = null): Lazy<VM> {
+    return viewModels(viewModelFactory?.let { { it } })
 }
 
-inline fun <reified VM : BaseViewModel> ComponentActivity.viewModels(viewModelFactory: ViewModelProvider.Factory): Lazy<VM> {
-    return viewModels { viewModelFactory }
+inline fun <reified VM : BaseViewModel> Fragment.getViewModels(viewModelFactory: ViewModelProvider.Factory? = null): Lazy<VM> {
+    return viewModels({ this }, viewModelFactory?.let { { it } })
 }
 
-inline fun <reified VM : BaseViewModel> Fragment.viewModels(viewModelFactory: ViewModelProvider.Factory): Lazy<VM> {
-    return viewModels { viewModelFactory }
-}
-
-inline fun <reified VM : BaseViewModel> Fragment.activityViewModels(viewModelFactory: ViewModelProvider.Factory): Lazy<VM> {
-    return activityViewModels { viewModelFactory }
-}
-
-inline fun <reified VM : BaseViewModel> ComponentActivity.savedStateViewModel(): Lazy<VM> {
-    return viewModels()
-}
-
-inline fun <reified VM : BaseViewModel> Fragment.savedStateViewModel(): Lazy<VM> {
-    return viewModels()
+inline fun <reified VM : BaseViewModel> Fragment.getActivityViewModels(viewModelFactory: ViewModelProvider.Factory? = null): Lazy<VM> {
+    return activityViewModels(viewModelFactory?.let { { it } })
 }
