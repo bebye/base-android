fe8142d2a2f8b89090f20814e22eaf86709ad8b1
mkwon <developer.mkwon@gmail.com>
2021-01-27 22:52:47 +0900

    update display metrics

--- base/src/main/kotlin/com/bebye/base/extension/ActivityExtension.kt
+++ base/src/main/kotlin/com/bebye/base/extension/ActivityExtension.kt
@@ -1,7 +1,12 @@
 package com.bebye.base.extension
 
+import android.graphics.Insets
+import android.os.Build
 import android.util.DisplayMetrics
+import android.view.WindowInsets
+import android.view.WindowMetrics
 import androidx.activity.ComponentActivity
+
 
 /**
  * Created by mkwon on 27/06/2020.
@@ -10,24 +15,34 @@ fun ComponentActivity.getDeviceRealWidth() = getRealDisplayMetrics().widthPixels
 
 fun ComponentActivity.getDeviceRealHeight() = getRealDisplayMetrics().heightPixels
 
-fun ComponentActivity.getDeviceWidth() = getDisplayMetrics().widthPixels
+fun ComponentActivity.getDeviceWidth() = getDisplaySize(false)
 
-fun ComponentActivity.getDeviceHeight() = getDisplayMetrics().heightPixels
+fun ComponentActivity.getDeviceHeight() = getDisplaySize(true)
 
 /**
  * with StatusBar and NavigationBar
  */
-fun ComponentActivity.getRealDisplayMetrics() : DisplayMetrics {
+fun ComponentActivity.getRealDisplayMetrics(): DisplayMetrics {
     val displayMetrics = DisplayMetrics()
-    windowManager.defaultDisplay.getRealMetrics(displayMetrics)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        display?.getRealMetrics(displayMetrics)
+    } else windowManager.defaultDisplay.getRealMetrics(displayMetrics)
     return displayMetrics
 }
 
 /**
  * without StatusBar and NavigationBar
  */
-fun ComponentActivity.getDisplayMetrics() : DisplayMetrics {
-    val displayMetrics = DisplayMetrics()
-    windowManager.defaultDisplay.getMetrics(displayMetrics)
-    return displayMetrics
+private fun ComponentActivity.getDisplaySize(isHeight: Boolean): Int {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val windowMetrics: WindowMetrics = windowManager.currentWindowMetrics
+        val insets: Insets = windowMetrics.windowInsets
+                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+        if (isHeight) windowMetrics.bounds.height() - insets.top - insets.bottom
+        else windowMetrics.bounds.width() - insets.left - insets.right
+    } else {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        if (isHeight) displayMetrics.heightPixels else displayMetrics.widthPixels
+    }
 }
